67bf9bf6ba91f94316fa26577e1c483b13af9a1a
jefferson <jeffer.leo93@gmail.com>
2020-12-04 08:35:52 -0600

    Cheque BAC

---
+++ report/report_payment.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, models
+import odoo.addons.l10n_gt_extra.a_letras
+
+class ReportAbstractPayment(models.AbstractModel):
+    _name = 'onlife.abstract.reporte_account_payment'
+
+    nombre_reporte = ''
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        self.model = 'account.payment'
+        docs = self.env[self.model].browse(docids)
+        return {
+            'doc_ids': docids,
+            'doc_model': self.model,
+            'docs': docs,
+            'a_letras': odoo.addons.l10n_gt_extra.a_letras,
+        }
+
+class ReportPayment1(models.AbstractModel):
+    _name = 'report.onlife.reporte_account_payment1'
+    _inherit = 'onlife.abstract.reporte_account_payment'
+
+    nombre_reporte = 'onlife.reporte_account_payment1'
